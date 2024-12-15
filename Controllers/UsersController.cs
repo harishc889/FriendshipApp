dbf8899f7ef62a0fe471d0173eb1c670c9c9e490
harishc889 <Harshitc889@gmail.com>
2024-12-15 12:43:34 +0530

    initial commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using FriendshipApp.Data;
+using FriendshipApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendshipApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context; 
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.User.ToList();
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.User.Find(id);
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using FriendshipApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FriendshipApp.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<AppUser> User { get; set; }
+    }
+}
